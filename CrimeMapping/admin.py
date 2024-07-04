d26faa6514caf7043db7aab8bd7be1e523b3fff8
Avinash Kumar <37548802+rock12231@users.noreply.github.com>
2024-07-04 16:39:27 +0530

    runing code upload

---
+++ CrimeMapping/admin.py
@@ -0,0 +1,75 @@
+from django.contrib import admin
+
+# Register your models here.
+from CrimeMapping.models import Crimes2001, ContactUs, GraphData, PoliceStationList ,ukdata, UKPoliceStationList, FirData, RajPolice, PoliceStationJaipurList, FirKarnataka, VictimInfo, RowdyShetters, ComplainantsDetails, MOBDetails, PoliceDetails
+
+class Crime(admin.ModelAdmin):
+    list_display = ('id','caseno', 'block', 'Type', 'Type_desc', 'Where', 'Arrest', 'Domestic', 'District', 'Community_area', 'Year', 'Latitude', 'Longitude', 'Date')
+admin.site.register(Crimes2001, Crime)
+
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('id','first_name', 'last_name', 'User_email', 'User_subject', 'User_message', 'date_created')
+admin.site.register(ContactUs, ContactAdmin)
+
+class GraphDataAdmin(admin.ModelAdmin):
+    list_display = ('id','created', 'title', 'Type', 'Where', 'Arrest', 'Domestic', 'District', 'YearS', 'YearE')
+admin.site.register(GraphData, GraphDataAdmin)
+
+class PoliceStationListAdmin(admin.ModelAdmin):
+    list_display = ('id','district', 'disname', 'address', 'city', 'zip', 'website', 'latitude', 'longitude')
+admin.site.register(PoliceStationList, PoliceStationListAdmin)
+
+#Rajasthan Police
+class UKPoliceAdmin(admin.ModelAdmin):
+    list_display = ('id','district', 'disname', 'address', 'city', 'zip', 'website', 'latitude', 'longitude')
+admin.site.register(UKPoliceStationList, UKPoliceAdmin)
+
+class ukAdmin(admin.ModelAdmin):
+     list_display = ('caseno', 'block', 'Type', 'Type_desc', 'Where', 'Arrest', 'Domestic', 'District', 'Community_area', 'Year', 'Latitude', 'Longitude', 'IPC','Session','Day','Date')
+admin.site.register(ukdata,ukAdmin )
+
+class FirDataAdmin(admin.ModelAdmin):
+     list_display = ('case_no', 'date', 'police_station', 'ipc', 'ipc_no', 'time', 'CrPC', 'date_happened', 'date_reported', 'format_fir_type', 'address',
+                    'distance_police_station', 'name', 'father_name', 'age', 'nationality', 'office_address', 'additional_info', 'suspected_individual', 
+                    'latitude', 'longitude', 'file', 'language')
+admin.site.register(FirData,FirDataAdmin)
+
+class RajPoliceAdmin(admin.ModelAdmin):
+     list_display = ('address', 'latitude', 'longitude')
+admin.site.register(RajPolice,RajPoliceAdmin)
+
+
+class PoliceStationJaipurListAdmin(admin.ModelAdmin):
+     list_display = ('Police_Station', 'Phone_Number','Address', 'Latitude', 'Longitude')
+admin.site.register(PoliceStationJaipurList,PoliceStationJaipurListAdmin)
+
+
+class FirKaranatakaAdmin(admin.ModelAdmin):
+    list_display = ('District_Name', 'UnitName', 'FIRNo', 'RI', 'Year', 'Month', 'Offence_From_Date', 'Offence_To_Date', 'FIR_Reg_DateTime', 'FIR_Date', 'FIR_Type', 'FIR_Stage', 'Complaint_Mode', 'CrimeGroup_Name', 'CrimeHead_Name', 'Latitude', 'Longitude','ActSection', 'IOName', 'KGID', 'Internal_IO', 'Place_of_Offence', 'Distance_from_PS', 'Beat_Name', 'Village_Area_Name', 'Male', 'Female', 'Boy', 'Girl', 'Age_0', 'VICTIM_COUNT', 'Accused_Count','Arrested_Male', 'Arrested_Female', 'Arrested_Count', 'Accused_ChargeSheeted_Count', 'Conviction_Count',   'FIR_ID', 'Unit_ID', 'Crime_No')
+admin.site.register(FirKarnataka, FirKaranatakaAdmin)
+
+class VictimInfoAdmin(admin.ModelAdmin):
+    list_display = ('District_Name', 'UnitName', 'FIRNo', 'Year', 'Month', 'VictimName', 'age', 'Caste', 'Profession', 'Sex', 'PresentAddress', 'PresentCity', 'PresentState', 'PermanentAddress', 'PermanentCity', 'PermanentState', 'Nationality_Name', 'DOB', 'PersonType', 'InjuryType', 'Injury_Nature', 'Crime_No', 'Arr_ID', 'Victim_ID')
+admin.site.register(VictimInfo, VictimInfoAdmin)
+
+
+class RowdyShettersAdmin(admin.ModelAdmin):
+    list_display = ('District_Name', 'Unit_Name', 'Rowdy_Sheet_No', 'Name', 'AliasName', 'RS_Open_Date', 'Rowdy_Classification_Details', 'Activities_Description', 'Rowdy_Category', 'PrevCase_Details', 'Address', 'Age', 'Father_Name', 'Source_Of_Income', 'LastUpdatedDate', 'PresentWhereabout')
+admin.site.register(RowdyShetters, RowdyShettersAdmin)
+
+class MOBDetailsAdmin(admin.ModelAdmin):
+    list_display = ('District_Name', 'Unit_Name', 'Name', 'Person_No', 'MOB_Number', 'MobOpenDate', 'MOB_Open_Year', 'Arrested_By', 'KGID', 'Caste', 'Grading', 'Occupation', 'PS_Native', 'PS_District', 'Offender_Class', 'Crime_No', 'ActSection', 'Brief_Fact', 'Present_WhereAbouts', 'Gang_Strength', 'Ident_Officer', 'officer_rank', 'Crime_Group1', 'Crime_Head2', 'class1', 'AGE', 'PresentAge', 'DOB', 'Address', 'SEX', 'Locality', 'LastUpdatedDate')
+
+admin.site.register(MOBDetails, MOBDetailsAdmin)
+
+
+class ComplainantsDetailsAdmin(admin.ModelAdmin):
+    list_display = ('District_Name', 'UnitName', 'FIRNo', 'Year', 'Month', 'ComplainantName', 'Relation', 'RelationshipName', 'DateOfBirth', 'Age', 'Sex', 'Nationality', 'Occupation', 'Address', 'City', 'State', 'Pincode', 'Caste', 'Religion', 'FIR_ID', 'Unit_ID', 'Complaint_ID')
+admin.site.register(ComplainantsDetails, ComplainantsDetailsAdmin)
+
+
+admin.site.register(PoliceDetails)
+
+
+
---
+++ Home/views.py
@@ -0,0 +1,179 @@
+from django.views import View
+from django.shortcuts import render 
+from CrimeMapping.views import ukdf, ukdfp, cards, cardData, graphData
+from CrimeMapping.views import getstaticmap
+import pandas as pd
+from CrimeMapping.models import PoliceStationJaipurList, FirKarnataka
+
+from CrimeAnalysis.models import Fir_Karnataka
+
+jaipur_police=pd.read_csv("CrimeMapping/data/jaipur_p.csv")
+# Create your views here.
+# dfk=pd.read_csv("CrimeMapping/data/FIR_Details.csv")
+
+class Home(View):
+    def get(self, request):
+        cardData()
+        graphData()
+
+        # # Import CSV file to database
+        # for index, row in dfk.iterrows():
+
+        #     data = {
+        #         'District_Name': row['District_Name'],
+        #         'UnitName': row['UnitName'],
+        #         'FIRNo': row['FIRNo'],
+        #         'RI': row['RI'],
+        #         'Year': row['Year'],
+        #         'Month': row['Month'],
+        #         'Offence_From_Date': row['Offence_From_Date'],
+        #         'Offence_To_Date': row['Offence_To_Date'],
+        #         'FIR_Reg_DateTime': row['FIR_Reg_DateTime'],
+        #         'FIR_Date': row['FIR_Date'],
+        #         'FIR_Type': row['FIR_Type'],
+        #         'FIR_Stage': row['FIR_Stage'],
+        #         'Complaint_Mode': row['Complaint_Mode'],
+        #         'CrimeGroup_Name': row['CrimeGroup_Name'],
+        #         'CrimeHead_Name': row['CrimeHead_Name'],
+        #         'Latitude': row['Latitude'],
+        #         'Longitude': row['Longitude'],
+        #         'ActSection': row['ActSection'],
+        #         'IOName': row['IOName'],
+        #         'KGID': row['KGID'],
+        #         'Internal_IO': row['Internal_IO'],
+        #         'Place_of_Offence': row['Place_of_Offence'],
+        #         'Distance_from_PS': row['Distance_from_PS'],
+        #         'Beat_Name': row['Beat_Name'],
+        #         'Village_Area_Name': row['Village_Area_Name'],
+        #         'Male': row['Male'],
+        #         'Female': row['Female'],
+        #         'Boy': row['Boy'],
+        #         'Girl': row['Girl'],
+        #         'Age_0': row['Age_0'],
+        #         'VICTIM_COUNT': row['VICTIM_COUNT'],
+        #         'Accused_Count': row['Accused_Count'],
+        #         'Arrested_Male': row['Arrested_Male'],
+        #         'Arrested_Female': row['Arrested_Female'],
+        #         'Arrested_Count': row['Arrested_Count_No'],
+        #         'Accused_ChargeSheeted_Count': row['Accused_ChargeSheeted_Count'],
+        #         'Conviction_Count': row['Conviction_Count'],
+        #         'FIR_ID': row['FIR_ID'],
+        #         'Unit_ID': row['Unit_ID'],
+        #         'Crime_No': row['Crime_No'],
+        #     }
+            
+        #     Fir_Karnataka.objects.create(**data)
+
+
+        #     # Create an instance of the Fir_Karnataka model
+        #     fir_karnataka = Fir_Karnataka(**data)
+            
+        #     # Save the instance to insert it into the database
+        #     fir_karnataka.save(using='custom_db')
+        #     print(index)
+
+
+        # for index, row in dfk.iterrows():
+
+        #     data = {
+        #         'District_Name': row['District_Name'],
+        #         'UnitName': row['UnitName'],
+        #         'FIRNo': row['FIRNo'],
+        #         'RI': row['RI'],
+        #         'Year': row['Year'],
+        #         'Month': row['Month'],
+        #         'Offence_From_Date': row['Offence_From_Date'],
+        #         'Offence_To_Date': row['Offence_To_Date'],
+        #         'FIR_Reg_DateTime': row['FIR_Reg_DateTime'],
+        #         'FIR_Date': row['FIR_Date'],
+        #         'FIR_Type': row['FIR_Type'],
+        #         'FIR_Stage': row['FIR_Stage'],
+        #         'Complaint_Mode': row['Complaint_Mode'],
+        #         'CrimeGroup_Name': row['CrimeGroup_Name'],
+        #         'CrimeHead_Name': row['CrimeHead_Name'],
+        #         'Latitude': row['Latitude'],
+        #         'Longitude': row['Longitude'],
+        #         'ActSection': row['ActSection'],
+        #         'IOName': row['IOName'],
+        #         'KGID': row['KGID'],
+        #         'Internal_IO': row['Internal_IO'],
+        #         'Place_of_Offence': row['Place_of_Offence'],
+        #         'Distance_from_PS': row['Distance_from_PS'],
+        #         'Beat_Name': row['Beat_Name'],
+        #         'Village_Area_Name': row['Village_Area_Name'],
+        #         'Male': row['Male'],
+        #         'Female': row['Female'],
+        #         'Boy': row['Boy'],
+        #         'Girl': row['Girl'],
+        #         'Age_0': row['Age_0'],
+        #         'VICTIM_COUNT': row['VICTIM_COUNT'],
+        #         'Accused_Count': row['Accused_Count'],
+        #         'Arrested_Male': row['Arrested_Male'],
+        #         'Arrested_Female': row['Arrested_Female'],
+        #         'Arrested_Count': row['Arrested_Count_No'],
+        #         'Accused_ChargeSheeted_Count': row['Accused_ChargeSheeted_Count'],
+        #         'Conviction_Count': row['Conviction_Count'],
+        #         'FIR_ID': row['FIR_ID'],
+        #         'Unit_ID': row['Unit_ID'],
+        #         'Crime_No': row['Crime_No'],
+        #     }
+            
+        #     FirKarnataka.objects.create(**data)
+
+        # tempDF = jaipur_police.copy()
+        # print(tempDF)
+        # print(type(tempDF))
+        # for i in range (len(tempDF)):
+        #     # Police_Station, Phone_Number,Address, Latitude,Longitude
+        #     data = {
+        #         'Police_Station' : tempDF['Police_Station'][i],	
+        #         'Phone_Number' : tempDF['Phone_Number'][i],
+        #         'Address' : tempDF['Address'][i],
+        #         'Latitude' : tempDF['Latitude'][i],
+        #         'Longitude' : tempDF['Longitude'][i],
+        #     }
+        #     PoliceStationJaipurList.objects.create(**data)
+        #     print(data,".......................")
+
+
+
+        # if request.user.is_authenticated:
+        return render(request, 'Home/home.html',{"cards":cards, "map": getstaticmap(400,None) }) 
+        # else:
+            # return redirect('/')
+        #Import CSV file to database
+
+
+    
+        #  Import CSV file to database RECORDS
+        # tempDF =ukdf.copy()
+        # for i in range (len(tempDF)):
+        #      #tempDF.unique(i)
+        #      data = {
+        #         'caseno' : tempDF['caseno'][i],	
+        #         'block' : tempDF['block'][i],
+        #         'Type' : tempDF['Type'][i],
+        #         'Type_desc' : tempDF['Type_desc'][i],
+        #         'Where' : tempDF['Where'][i],
+        #         'Arrest' : tempDF['Arrest'][i],
+        #         'Domestic' : tempDF['Domestic'][i],
+        #         'District' : tempDF['District'][i],
+        #         'Community_area' : tempDF['Community_area'][i],
+        #         'Year' : tempDF['Year'][i],
+        #         'Latitude' : tempDF['Latitude'][i],
+        #         'Longitude' : tempDF['Longitude'][i],
+        #         'IPC':tempDF['IPC'][i],
+        #         'Session':tempDF['Session'][i],
+        #         'Day':tempDF['Day'][i],
+
+        #     }
+        #      print(data,".......................")
+
+
+        #      ukdata.objects.create(**data)
+        
+        # return render(request, 'Home/home.html',{"cards":cards}) 
+
+    def post(self, request):
+        pass
+
